727b59fb48499f7334ca4900620ea379971d14fc
Norbert Schlia <nschlia@oblivion-software.de>
2018-10-30 16:41:10 +0100

    Reformat

--- src/ffmpeg_utils.cc
+++ src/ffmpeg_utils.cc
@@ -1004,7 +1004,7 @@ void exepath(string * path)
 std::string &ltrim(std::string &s)
 {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))));
+                                    std::not1(std::ptr_fun<int, int>(std::isspace))));
     return s;
 }
 
@@ -1012,7 +1012,7 @@ std::string &ltrim(std::string &s)
 std::string &rtrim(std::string &s)
 {
     s.erase(std::find_if(s.rbegin(), s.rend(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+                         std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
     return s;
 }
 

5fcf2e1986d24eec445b6b29ed5fbdb1a25d0186
Norbert Schlia <nschlia@oblivion-software.de>
2018-12-17 10:47:23 +0100

    Speed up DVD handling

--- src/dvdparser.cc
+++ src/dvdparser.cc
@@ -34,6 +34,92 @@
 //#include <dvdread/nav_read.h>
 //#include <dvdread/nav_print.h>
 
+static int dvd_find_best_audio_stream(vtsi_mat_t *vtsi_mat, int *best_channels, int *best_sample_frequency);
+
+static int dvd_find_best_audio_stream(vtsi_mat_t *vtsi_mat, int *best_channels, int *best_sample_frequency)
+{
+    int best_stream = -1;
+    int best_application_mode = INT_MAX;
+    int best_lang_extension = INT_MAX;
+    int best_quantization = 0;
+
+    *best_channels = 0;
+    *best_sample_frequency = 0;
+
+    for(int i = 0; i < vtsi_mat->nr_of_vts_audio_streams; i++)
+    {
+        audio_attr_t *attr = &vtsi_mat->vts_audio_attr[i];
+
+        if(attr->audio_format == 0
+                && attr->multichannel_extension == 0
+                && attr->lang_type == 0
+                && attr->application_mode == 0
+                && attr->quantization == 0
+                && attr->sample_frequency == 0
+                && attr->unknown1 == 0
+                && attr->channels == 0
+                && attr->lang_extension == 0
+                && attr->unknown3 == 0) {
+            // Unspecified
+            continue;
+        }
+
+        // Preference in this order, if higher value is same, compare next and so on.
+        //
+        // application_mode: prefer not specified.
+        //  0: not specified
+        //  1: karaoke mode
+        //  2: surround sound mode
+        // lang_extension: prefer not specified or normal audio
+        //  0: Not specified
+        //  1: Normal audio/Caption
+        //  2: visually impaired
+        //  3: Director's comments 1
+        //  4: Director's comments 2
+        // sample_frequency: maybe 48K only
+        //  0: 48kHz
+        //  1: ??kHz
+        // quantization prefer highest bit width or drc
+        //  0: 16bit
+        //  1: 20bit
+        //  2: 24bit
+        //  3: drc
+        // channels: prefer no extension
+        //  multichannel_extension
+
+        //        if ((best_multiframe >  multiframe) ||
+        //        (best_multiframe == multiframe && best_bitrate >  bitrate) ||
+        //        (best_multiframe == multiframe && best_bitrate == bitrate && best_count >= count))
+
+        // Specs keep the meaning of the values of this field other than 0 secret, so we nail it to 48 kHz.
+        int sample_frequency = 48000;
+
+        if ((best_application_mode < attr->application_mode) ||
+                (best_application_mode == attr->application_mode && best_lang_extension < attr->lang_extension) ||
+                (best_application_mode == attr->application_mode && best_lang_extension == attr->lang_extension && *best_sample_frequency > sample_frequency) ||
+                (best_application_mode == attr->application_mode && best_lang_extension == attr->lang_extension && *best_sample_frequency == sample_frequency && *best_channels > attr->channels) ||
+                (best_application_mode == attr->application_mode && best_lang_extension == attr->lang_extension && *best_sample_frequency == sample_frequency && *best_channels == attr->channels && best_quantization > attr->quantization)
+                )
+        {
+            continue;
+        }
+
+        best_stream             = i;
+        best_application_mode   = attr->application_mode;
+        best_lang_extension     = attr->lang_extension;
+        *best_sample_frequency  = sample_frequency;
+        *best_channels          = attr->channels;
+        best_quantization       = attr->quantization;
+    }
+
+    if (best_stream > -1)
+    {
+        ++*best_channels;
+    }
+
+    return best_stream;
+}
+
 int parse_dvd(const std::string & path, const struct stat *statbuf, void *buf, fuse_fill_dir_t filler)
 {
     dvd_reader_t *dvd;
@@ -89,6 +175,52 @@ int parse_dvd(const std::string & path, const struct stat *statbuf, void *buf, f
             return EINVAL;
         }
 
+        // Set reasonable defaults
+        int64_t audio_bit_rate = 256000;
+        int channels = 2;
+        int sample_rate = 48000;
+        int audio = 0;
+
+        BITRATE video_bit_rate = 8000000;
+        int width = 720;
+        int height = 576;
+
+        if (vts_file->vtsi_mat)
+        {
+            audio = dvd_find_best_audio_stream(vts_file->vtsi_mat, &channels, &sample_rate);
+
+            height = (vts_file->vtsi_mat->vts_video_attr.video_format != 0) ? 576 : 480;
+
+            switch(vts_file->vtsi_mat->vts_video_attr.picture_size)
+            {
+            case 0:
+            {
+                width = 720;
+                break;
+            }
+            case 1:
+            {
+                width = 704;
+                break;
+            }
+            case 2:
+            {
+                width = 352;
+                break;
+            }
+            case 3:
+            {
+                width = 352;
+                height /= 2;
+                break;
+            }
+            default:
+            {
+                Logging::warning(path, "DVD video contains invalid picture size atttribute.");
+            }
+            }
+        }
+
         vts_ptt_srpt = vts_file->vts_ptt_srpt;
         for( j = 0; j < chapts; ++j )
         {
@@ -142,6 +274,34 @@ int parse_dvd(const std::string & path, const struct stat *statbuf, void *buf, f
                 virtualfile->dvd.m_title_no     = title_no;
                 virtualfile->dvd.m_chapter_no   = chapter_no;
                 virtualfile->dvd.m_angle_no     = 1;            // TODO
+
+                if (!transcoder_cached_filesize(virtualfile, &stbuf))
+                {
+                    int first_cell = start_cell;
+
+                    // Check if we're entering an angle block.
+                    if (cur_pgc->cell_playback[ first_cell ].block_type == BLOCK_TYPE_ANGLE_BLOCK)
+                    {
+                        first_cell += virtualfile->dvd.m_angle_no;
+                    }
+
+                    double frame_rate = (((cur_pgc->cell_playback[ first_cell ].playback_time.frame_u & 0xc0) >> 6) == 1) ? 25 : 29.97;
+                    int64_t frac = static_cast<int64_t>((cur_pgc->cell_playback[ first_cell ].playback_time.frame_u & 0x3f) * AV_TIME_BASE / frame_rate);
+                    int64_t msecduration = static_cast<int64_t>((cur_pgc->cell_playback[ first_cell ].playback_time.hour * 60 + cur_pgc->cell_playback[ first_cell ].playback_time.minute) * 60 + cur_pgc->cell_playback[ first_cell ].playback_time.second) * AV_TIME_BASE + frac;
+                    uint64_t size = (cur_pgc->cell_playback[ first_cell ].last_sector - cur_pgc->cell_playback[ first_cell ].first_sector) * 2048;
+                    int interleaved = cur_pgc->cell_playback[ first_cell ].interleaved;
+                    double duration = static_cast<double>(msecduration) / AV_TIME_BASE;
+
+                    video_bit_rate = static_cast<BITRATE>(static_cast<double>(size) * 8 / duration);   // calculate bitrate in bps
+
+                    Logging::debug(virtualfile->m_origfile, "Video Bit Rate: %1 Dimensions: %2x%3@%<%5.2f>4 fps Interleaved: %5 Size: %6", format_bitrate(video_bit_rate), width, height, frame_rate, interleaved ? "yes" : "no", format_size(size));
+                    if (audio > -1)
+                    {
+                        Logging::debug(virtualfile->m_origfile, "Audio Channels: %1 Sample Rate: %2", channels, sample_rate);
+                    }
+
+                    transcoder_set_filesize(virtualfile, duration, audio_bit_rate, channels, sample_rate, video_bit_rate, width, height, interleaved, frame_rate);
+                }
             }
         }
 

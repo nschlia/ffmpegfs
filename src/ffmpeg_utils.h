01248d80bb699b2a92956b2547cd1c64fac4d23e
Norbert Schlia <nschlia@oblivion-software.de>
2019-01-25 21:01:27 +0100

    Display fractional seconds in duration with 3 digits

--- src/ffmpeg_utils.h
+++ src/ffmpeg_utils.h
@@ -247,7 +247,7 @@ double              ffmpeg_cvttime(int64_t ts, const AVRational & time_base);
 std::string         format_number(int64_t value);
 std::string         format_bitrate(BITRATE value);
 std::string         format_samplerate(unsigned int value);
-std::string         format_duration(int64_t value, int fracs = 1);
+std::string         format_duration(int64_t value, int fracs = 3);
 std::string         format_time(time_t value);
 std::string         format_size(size_t value);
 std::string         format_size_ex(size_t value);

60c9ae1a0cb5d28667eb45181ea5f20b4fe7c7e7
Norbert Schlia <nschlia@oblivion-software.de>
2022-05-31 23:11:40 +0200

    Closes #138: Fix gcc-12 warning

--- src/ffmpeg_utils.cc
+++ src/ffmpeg_utils.cc
@@ -1786,13 +1786,13 @@ void exepath(std::string * path)
 
 std::string &ltrim(std::string &s)
 {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not_fn(std::ptr_fun<int, int>(std::isspace))));
     return s;
 }
 
 std::string &rtrim(std::string &s)
 {
-    s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    s.erase(std::find_if(s.rbegin(), s.rend(), std::not_fn(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
     return s;
 }
 
